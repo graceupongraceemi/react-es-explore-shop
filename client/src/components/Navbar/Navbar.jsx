2c17acaaabcf9c6e3f87bd6ae63bff61c02d3569
Emiko Esther S <graceupongraceemi@gmail.com>
2023-01-17 12:17:10 -0800

    sass, @mui/material, and @mui/icons-material are installed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='navbar'>
+      <div className='wrapper'>
+        <div className='leftr'>
+          <div>
+            <img src='/img/en.png' alt='' />
+            <KeyboardArrowDownIcon />
+          </div>
+        </div>
+        <div className='center'></div>
+        <div className='right'></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
