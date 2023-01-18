e332b09a9405ff8d72060f59119c17e8727c019a
Emiko Esther S <graceupongraceemi@gmail.com>
2023-01-18 00:06:21 -0800

    New components, Footer, Slider, Home, Product along with scss files created. Slider.jsx done for Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
+import { Children } from 'react';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Products from './pages/Products/Products';
 import Product from './pages/Product/Product';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import './app.scss';
 
 const Layout = () => {
   return (
-    <div>
+    <div className='app'>
       <Navbar />
       <Outlet />
       <Footer />
