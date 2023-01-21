54bd3631bf6c73aaec4d08bcef5a5fca6ff7021f
Emiko Esther S <graceupongraceemi@gmail.com>
2023-01-20 16:44:13 -0800

    Slider.scss modified. Products.jsx from pages directory is modified with Product Categories in  Product Categories, filter by price, and Sort by

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -2,7 +2,39 @@ import React from 'react';
 import './Products.scss';
 
 const Products = () => {
-  return <div className='products'>Products</div>;
+  return (
+    <div className='products'>
+      <div className='left'>
+        <div className='filterItem'>
+          <h2>Product Categories</h2>
+          <div className='inputItem'>
+            <input type='checkbox' id='1' value={1} />
+            <label htmlFor='1'>Shoes</label>
+          </div>
+          <div className='inputItem'>
+            <input type='checkbox' id='2' value={2} />
+            <label htmlFor='1'>Skirts</label>
+          </div>
+          <div className='inputItem'>
+            <input type='checkbox' id='3' value={3} />
+            <label htmlFor='1'>Coats</label>
+          </div>
+        </div>
+        <div className='filterItem'>
+          <h2>Filter by price</h2>
+          <div className='inputItem'>
+            <span>0</span>
+            <input type='range' min={0} max={1000} />
+            <span>1000</span>
+          </div>
+        </div>
+        <div className='filterItem'>
+          <h2>Sort by</h2>
+        </div>
+      </div>
+      <div className='right'></div>
+    </div>
+  );
 };
 
 export default Products;
