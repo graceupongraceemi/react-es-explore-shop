d34440be84e0892ff5049c65237a5bdef3263159
Emiko Esther S <graceupongraceemi@gmail.com>
2023-01-20 17:15:52 -0800

    img added

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -40,7 +40,13 @@ const Products = () => {
           </div>
         </div>
       </div>
-      <div className='right'></div>
+      <div className='right'>
+        <img
+          className='catImg'
+          src='https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600'
+          alt=''
+        />
+      </div>
     </div>
   );
 };
