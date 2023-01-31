f9670a1941925c0b0743bcb3fe4b9bfd72fca6c6
Emiko Esther S <graceupongraceemi@gmail.com>
2023-01-31 12:44:01 -0800

    in List.jsx, sub_categories filter added according to Strapi operator

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -5,7 +5,9 @@ import useFetch from '../../hooks/useFetch';
 
 const List = ({ subCats, maxPrice, sort, catId }) => {
   const { data, loading, error } = useFetch(
-    `/products?populate=*&[filters][categories][id]=${catId}`
+    `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}`
   );
   return (
     <div className='list'>
